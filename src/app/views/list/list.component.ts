4870e73bb1a627b08419bcce3edb9537e5244ecf
Aida Monfort Muriach <aida.monfort@uji.es>
2020-05-26 09:30:27 +0200

    provide sorting options and number of search results

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -24,6 +24,10 @@ export class ListComponent implements OnInit {
   competencesFilter: Boolean = true;
   isAnonymous = null;
   currentUser: User = new User();
+  sortNameAsc = true;
+  sortOrgAsc = true;
+  sortUpdAsc = true;
+  sortedBy = 'lastUpdated';
 
   @ViewChild('dangerModal') public dangerModal: ModalDirective;
 
@@ -118,5 +122,30 @@ export class ListComponent implements OnInit {
         });
       }
     });
+  }
+
+  sortBy(attr) {
+  //  this.paginationLimitFrom = 0;
+  //  this.paginationLimitTo = 6;
+  //  this.currentPage = 0;
+    switch (attr) {
+      case 'name':
+        this.sortNameAsc = !this.sortNameAsc;
+        this.sortedBy = 'name';
+        // tslint:disable-next-line:max-line-length
+        this.filteredJobOffers.sort((a, b) => (a.occuProf.title.toLowerCase() > b.occuProf.title.toLowerCase()) ? this.sortNameAsc ? 1 : -1 : this.sortNameAsc ? -1 : 1);
+        break;
+      case 'lastUpdated':
+        this.sortUpdAsc = !this.sortUpdAsc;
+        this.sortedBy = 'lastUpdated';
+        this.filteredJobOffers.sort((a, b) => (a.updatedAt > b.updatedAt) ? this.sortUpdAsc ? 1 : -1 : this.sortUpdAsc ? -1 : 1);
+        break;
+      case 'organization':
+        this.sortOrgAsc = !this.sortOrgAsc;
+        this.sortedBy = 'organization';
+        // tslint:disable-next-line:max-line-length
+        this.filteredJobOffers.sort((a, b) => (a.orgName.toLowerCase() > b.orgName.toLowerCase()) ? this.sortOrgAsc ? 1 : -1 : this.sortOrgAsc ? -1 : 1);
+        break;
+    }
   }
 }
